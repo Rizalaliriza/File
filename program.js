5e654ed1ca6ad99417d4ce675d7f742186c4525a
Rizalaliriza <97432451+Rizalaliriza@users.noreply.github.com>
2024-11-28 14:57:59 +0700

    JavaScript
    Oke

---
+++ program.js
@@ -0,0 +1,8 @@
+var h = prompt("Yakin mau masuk web saya;");
+var n = confirm("Hai " + c + "! Yakin Mau Masuk?");
+
+if (n == true) {
+  alert('web eeroor!!');
+} else {
+  alert('Kena hack?!!');
+}
